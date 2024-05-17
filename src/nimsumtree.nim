d52f75b272bef3a1253914abfcd3d1f67d2e89e8
Nico <nicolas.otremba@web.com>
2024-05-17 22:55:42 +0200

    Progress

--- src/nimsumtree.nim
+++ src/nimsumtree.nim
@@ -1,7 +1,58 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+import std/[macros]
 
-proc add*(x, y: int): int =
-  ## Adds two numbers together.
-  return x + y
+
+type
+  Arc*[T] = object
+    value: ref T
+
+  Summary* {.explain.} = concept var a, b
+    a += b
+
+  Item* {.explain.} = concept a, type T
+    a.summary is Summary
+
+template summaryType*(T: typedesc[Item]): untyped = typeof(T.default.summary)
+
+type
+  NodeKind* = enum Internal, Leaf
+  Node*[T: Item] = object
+    case kind*: NodeKind
+    of Internal:
+      children: seq[Arc[Node[T]]]
+      summaries: seq[T.summaryType]
+    of Leaf:
+      summary: T.summaryType
+      data: seq[T]
+
+  SumTree*[T: Item] = Node[T]
+
+# func newLeaf[T
+
+when isMainModule:
+  type TestSummary = object
+    count: int
+    max: int
+    zeroes: int
+
+  type TestSummary2 = object
+    count: int
+    max: int
+    ones: int
+
+  # func summaryType*(x: typedesc[int]): typedesc[Summary] = TestSummary
+
+  func `+=`(a: var TestSummary, b: TestSummary) =
+    a.count += b.count
+    a.max = max(a.max, b.max)
+    a.zeroes += b.zeroes
+
+  func `+=`(a: var TestSummary2, b: TestSummary2) =
+    a.count += b.count
+    a.max = max(a.max, b.max)
+    a.ones += b.ones
+
+  func summary(x: int): TestSummary = TestSummary(count: 1, max: x, zeroes: if x == 0: 1 else: 0)
+  # func summary(x: int): TestSummary2 = TestSummary2(count: 1, max: x, ones: if x == 1: 1 else: 0)
+
+  var node = SumTree[int](kind: Leaf)
+  echo node

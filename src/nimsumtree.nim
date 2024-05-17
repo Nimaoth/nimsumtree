58b58fc3086d352f6064234b72c8ccf130da65f9
Nico <nicolas.otremba@web.com>
2024-05-18 00:57:34 +0200

    Basic tree construction from item array

--- src/nimsumtree.nim
+++ src/nimsumtree.nim
@@ -1,5 +1,4 @@
-import std/[macros, strformat]
-
+import std/[macros, strformat, sequtils, options, strutils]
 
 type
   Arc*[T] = object
@@ -11,9 +10,13 @@ type
   Item* {.explain.} = concept a, type T
     a.summary is Summary
 
+proc `=copy`*[T](a: var Arc[T], b: Arc[T]) {.error.}
+proc `=dup`*[T](a: Arc[T]): Arc[T] {.error.}
+
 template summaryType*(T: typedesc[Item]): untyped = typeof(T.default.summary)
 
-const treeBase = 6
+const treeBase = 1
+const treeChildrenMax = treeBase * 2
 
 type
   NodeKind* = enum Internal, Leaf
@@ -30,8 +33,27 @@ type
   SumTree*[T: Item] = distinct Arc[Node[T]]
 
 func `$`*[T](arc: Arc[T]): string = &"Arc({arc.value[]})"
-func `$`*[T: Item](node: Node[T]): string = &"Node({node.kind}, {node.mSummary})"
+func `$`*[T: Item](node: Node[T]): string =
+  case node.kind:
+  of Internal:
+    &"Internal({node.mSummary}, {node.mChildren})"
+  of Leaf:
+    &"Leaf({node.mSummary}, {node.mItems})"
 func `$`*[T: Item](tree: SumTree[T]): string = $Arc[Node[T]](tree)
+
+func pretty*[T: Item](node {.byref.}: Node[T]): string =
+  case node.kind:
+  of Internal:
+    result = &"Internal({node.mSummary}, {node.mChildren.len}):\n"
+    for i in 0..node.mChildren.high:
+      if i > 0:
+        result.add "\n"
+      result.add node.mChildren[i].value[].pretty.indent(2)
+
+  of Leaf:
+    result = &"Leaf({node.mSummary}, {node.mItems})"
+
+func pretty*[T: Item](tree: SumTree[T]): string = Arc[Node[T]](tree).value[].pretty
 
 func isLeaf*[T: Item](node: Node[T]): bool = node.kind == Leaf
 func isInternal*[T: Item](node: Node[T]): bool = node.kind == Internal
@@ -76,6 +98,55 @@ func new*[T](_: typedesc[Arc[T]], default: sink T): Arc[T] =
 func new*[T: Item](_: typedesc[SumTree[T]]): SumTree[T] =
   SumTree[T](Arc[Node[T]].new(newLeaf[T]()))
 
+func new*[T: Item](_: typedesc[SumTree[T]], items: openArray[T]): SumTree[T] =
+  mixin summary
+  mixin `+=`
+
+  var nodes: seq[Node[T]]
+  var i = 0
+  while i < items.len:
+    let endIndex = min(i + treeChildrenMax, items.len)
+    let subItems: seq[T] = items[i..<endIndex]
+    i = endIndex
+
+    let summaries: seq[T.summaryType] = subItems.mapIt(it.summary)
+    var s: T.summaryType = summaries[0]
+    for k in 1..summaries.high:
+      s += summaries[k]
+
+    nodes.add Node[T](kind: Leaf, mSummary: s, mItems: subItems, mSummaries: summaries)
+
+  var parentNodes: seq[Node[T]] = @[]
+  var height: uint8 = 0
+  while nodes.len > 1:
+    inc height
+    var currentParentNode = Node[T].none
+    var tempNodes = nodes.move
+    for childNode in tempNodes.mitems:
+      if currentParentNode.isNone:
+        currentParentNode = some Node[T](kind: Internal, mSummary: T.summaryType.default, mHeight: height)
+
+      let childSummary = childNode.summary
+      currentParentNode.get.mSummary += childSummary
+      currentParentNode.get.mSummaries.add childSummary
+      currentParentNode.get.mChildren.add Arc[Node[T]].new(childNode.move)
+
+      if currentParentNode.get.mChildren.len == treeChildrenMax:
+        parentNodes.add currentParentNode.get.move
+        currentParentNode = Node[T].none
+
+    if currentParentNode.isSome:
+      parentNodes.add currentParentNode.get.move
+      currentParentNode = Node[T].none
+
+    nodes = parentNodes.move
+
+  if nodes.len == 0:
+    SumTree[T](Arc[Node[T]].new(newLeaf[T]()))
+  else:
+    assert nodes.len == 1
+    SumTree[T](Arc[Node[T]].new(nodes[0].move))
+
 when isMainModule:
   type TestSummary = object
     count: int
@@ -87,7 +158,7 @@ when isMainModule:
     max: int
     ones: int
 
-  func `+=`(a: var TestSummary, b: TestSummary) =
+  func `+=`*(a: var TestSummary, b: TestSummary) =
     a.count += b.count
     a.max = max(a.max, b.max)
     a.zeroes += b.zeroes
@@ -97,19 +168,7 @@ when isMainModule:
     a.max = max(a.max, b.max)
     a.ones += b.ones
 
-  func summary(x: int): TestSummary = TestSummary(count: 1, max: x, zeroes: if x == 0: 1 else: 0)
-  # func summary(x: int): TestSummary2 = TestSummary2(count: 1, max: x, ones: if x == 1: 1 else: 0)
+  func summary*(x: int): TestSummary = TestSummary(count: 1, max: x, zeroes: if x == 0: 1 else: 0)
 
-  var node = newLeaf[int]()
-  echo node
-  echo node.isLeaf
-  echo node.isInternal
-  echo node.summary
-  echo "---"
-
-  for s in node.childSummaries:
-    echo s
-
-  echo "---"
-  var tree = SumTree[int].new()
-  echo $tree
+  var tree = SumTree[int].new([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+  echo tree.pretty
